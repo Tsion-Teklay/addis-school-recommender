8f3c927eb4010f15abf65a2d7743d68a71f02758
Tsion Teklay <tsionteklay18@gmail.com>
2026-02-28 09:54:58 +0300

    added config with subcity coordinates and constants

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# Addis Ababa sub-city coordinates (synthetic for scoring)
+
+SUBCITY_COORDINATES = {
+    "Bole": (8, 9),
+    "Yeka": (7, 10),
+    "Arada": (5, 8),
+    "Kirkos": (6, 7),
+    "Lideta": (4, 7),
+    "Kolfe Keranio": (3, 6),
+    "Nifas Silk-Lafto": (5, 5),
+    "Gullele": (6, 11),
+    "Akaky Kaliti": (9, 3),
+    "Addis Ketema": (4, 8)
+}
+
+SCHOOL_TYPES = ["Public", "Private", "International"]
+STREAMS = ["Science", "Social", "General"]
